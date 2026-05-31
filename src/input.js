97a10872dc0b5c2427266cc774ce0e889257087a
Ajit J Gupta <ajit.gupta@hingehealth.com>
2026-05-31 10:44:07 +0530

    fix: selective preventDefault and add destroy() to KeyboardInput

--- src/input.js
+++ src/input.js
@@ -1,16 +1,29 @@
+const GAME_KEYS = new Set([
+  'Space', 'Enter',
+  'ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown',
+  'KeyA', 'KeyD', 'KeyW', 'KeyS',
+]);
+
 export class KeyboardInput {
   constructor() {
     this._keys = new Set();
-    window.addEventListener('keydown', e => {
+    this._onKeyDown = e => {
       this._keys.add(e.code);
-      e.preventDefault();
-    });
-    window.addEventListener('keyup', e => {
+      if (GAME_KEYS.has(e.code)) e.preventDefault();
+    };
+    this._onKeyUp = e => {
       this._keys.delete(e.code);
-    });
+    };
+    window.addEventListener('keydown', this._onKeyDown);
+    window.addEventListener('keyup', this._onKeyUp);
   }
 
   isDown(code) {
     return this._keys.has(code);
+  }
+
+  destroy() {
+    window.removeEventListener('keydown', this._onKeyDown);
+    window.removeEventListener('keyup', this._onKeyUp);
   }
 }
